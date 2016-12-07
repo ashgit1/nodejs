1f858adf9d418f679b213a67c1d225aea925129d
Ashish Kumar <a0k00ee@m-C02S5DXEG8WN.local>
2016-12-07 14:21:53 +0530

    Added Learnings for Scoped Packages and Tags along with minor patch version of ash u-npm-demp-pkg module

--- ashu-npm-demo-pkg/index.js
+++ ashu-npm-demo-pkg/index.js
@@ -1,5 +1,5 @@
 var colors = require('colors');
 
 exports.printMsg = function(){
-  console.log("This message is from ashu-npm-demo-pkg module".red);
+  console.log("This message is from ashu-npm-demo-pkg module".green);
 }
